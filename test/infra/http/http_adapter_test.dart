be07fa7d69f32c0f5a3ee57cd7a9940d2c649f00
César Marino (casa) <cesar_marino@live.com>
2020-08-12 19:58:59 -0300

    ensure HttpAdapter returns null if post returns 204 with data

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -25,7 +25,11 @@ class HttpAdapter implements HttpClient {
 
     final jsonBody = body != null ? jsonEncode(body) : null;
     final response = await client.post(url, headers: headers, body: jsonBody);
-    return response.body.isEmpty ? null : jsonDecode(response.body);
+
+    if (response.statusCode == 200)
+      return response.body.isEmpty ? null : jsonDecode(response.body);
+    else
+      return null;
   }
 }
 
@@ -92,6 +96,12 @@ void main() {
 
     test('Shuld return null if post returns 204', () async {
       mockResponse(204, body: '');
+      var response = await sut.request(url: url, method: 'post');
+      expect(response, null);
+    });
+
+    test('Shuld return null if post returns 204 with data', () async {
+      mockResponse(204);
       var response = await sut.request(url: url, method: 'post');
       expect(response, null);
     });
