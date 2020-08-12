c8e77622244cbe3eeb76b81b3e202ac4bb665571
César Marino (casa) <cesar_marino@live.com>
2020-08-12 19:39:42 -0300

    ensure HttpAdapter returns data if post returns 200

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -6,12 +6,14 @@ import 'package:test/test.dart';
 import 'package:http/http.dart';
 import 'package:meta/meta.dart';
 
-class HttpAdapter {
+import 'package:curso/data/http/http.dart';
+
+class HttpAdapter implements HttpClient {
   final Client client;
 
   HttpAdapter(this.client);
 
-  Future<void> request({
+  Future<Map> request({
     @required String url,
     @required String method,
     Map body,
@@ -22,7 +24,8 @@ class HttpAdapter {
     };
 
     final jsonBody = body != null ? jsonEncode(body) : null;
-    await client.post(url, headers: headers, body: jsonBody);
+    final response = await client.post(url, headers: headers, body: jsonBody);
+    return jsonDecode(response.body);
   }
 }
 
@@ -41,6 +44,10 @@ void main() {
 
   group('post', () {
     test('Shuld call post with correct values', () async {
+      when(
+        client.post(any, headers: anyNamed('headers'), body: anyNamed('body')),
+      ).thenAnswer((_) async => Response('{"any_key":"any_value"}', 200));
+
       await sut.request(
         url: url,
         method: 'post',
@@ -58,8 +65,22 @@ void main() {
     });
 
     test('Shuld call post without body', () async {
+      when(
+        client.post(any, headers: anyNamed('headers'), body: anyNamed('body')),
+      ).thenAnswer((_) async => Response('{"any_key":"any_value"}', 200));
+
       await sut.request(url: url, method: 'post');
       verify(client.post(any, headers: anyNamed('headers')));
+    });
+
+    test('Shuld return data if post returns 200', () async {
+      when(client.post(any, headers: anyNamed('headers'))).thenAnswer(
+        (_) async => Response('{"any_key":"any_value"}', 200),
+      );
+
+      var response = await sut.request(url: url, method: 'post');
+
+      expect(response, {'any_key': 'any_value'});
     });
   });
 }
