f867e054873f605613615c2a4c4ea5e8a7086bee
César Marino (casa) <cesar_marino@live.com>
2020-08-13 20:17:55 -0300

    change StatusBar to light mode

--- lib/ui/components/app.dart
+++ lib/ui/components/app.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../pages/pages.dart';
 
 class App extends StatelessWidget {
-  final primaryColor = Color.fromRGBO(136, 14, 79, 1);
-  final primaryColorDark = Color.fromRGBO(96, 0, 39, 1);
-  final primaryColorLight = Color.fromRGBO(188, 71, 123, 1);
-
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+    final primaryColor = Color.fromRGBO(136, 14, 79, 1);
+    final primaryColorDark = Color.fromRGBO(96, 0, 39, 1);
+    final primaryColorLight = Color.fromRGBO(188, 71, 123, 1);
+
     return MaterialApp(
       title: '4Dev',
       debugShowCheckedModeBanner: false,
