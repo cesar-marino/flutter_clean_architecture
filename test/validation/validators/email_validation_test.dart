2f6e2f977466ba08e94f98f65c2c67752ec70c56
César Marino (casa) <cesar_marino@live.com>
2020-08-17 21:41:33 -0300

    ensure EmailValidation returns null if email is valid

--- test/validation/validators/email_validation_test.dart
+++ test/validation/validators/email_validation_test.dart
@@ -1,3 +1,4 @@
+import 'package:faker/faker.dart';
 import 'package:test/test.dart';
 
 import 'package:curso/validation/protocols/protocols.dart';
@@ -26,5 +27,9 @@ void main() {
 
   test('Shold return null if email is null', () {
     expect(sut.validate(null), null);
+  });
+
+  test('Shold return null if email is valid', () {
+    expect(sut.validate(faker.internet.email()), null);
   });
 }
