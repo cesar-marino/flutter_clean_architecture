419b7c0f052033640c3fa99c8f2241178f9dc394
César Marino (casa) <cesar_marino@live.com>
2020-08-26 19:38:25 -0300

    ensure RemoteAddAccount throws UnexpectedErroron 404

--- test/data/usecases/add_account/remote_add_account_test.dart
+++ test/data/usecases/add_account/remote_add_account_test.dart
@@ -58,4 +58,12 @@ void main() {
 
     expect(future, throwsA(DomainError.unexpected));
   });
+
+  test('Shold throw UnexpectedError if HttpClient return 404', () async {
+    mockHttpError(HttpError.notFound);
+
+    final future = sut.add(params);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
 }
