3a26b8e647e2821e4e0dc0c3df0fe7caa16472fd
César Marino (casa) <cesar_marino@live.com>
2020-08-28 20:39:07 -0300

    ensure GetxSignUpPresenter emits error if addAccount fails

--- test/presentation/presenters/getx_signup_presenter_test.dart
+++ test/presentation/presenters/getx_signup_presenter_test.dart
@@ -42,6 +42,10 @@ void main() {
     mockAddAccountCall().thenAnswer((_) async => AccountEntity(token));
   }
 
+  void mockAddAccountError(DomainError error) {
+    mockAddAccountCall().thenThrow(error);
+  }
+
   PostExpectation mockSaveCurrentAccountCall() =>
       when(saveCurrentAccount.save(any));
 
@@ -334,6 +338,38 @@ void main() {
     sut.validatePasswordConfirmation(passwordConfirmation);
 
     expectLater(sut.isLoadingStream, emits(true));
+
+    await sut.signUp();
+  });
+
+  test('Shold emit correct events on EmailInUseError', () async {
+    mockAddAccountError(DomainError.emailInUse);
+    sut.validateName(name);
+    sut.validateEmail(email);
+    sut.validatePassword(password);
+    sut.validatePasswordConfirmation(passwordConfirmation);
+
+    expectLater(sut.isLoadingStream, emitsInOrder([true, false]));
+
+    sut.mainErrorStream.listen(
+      expectAsync1((error) => expect(error, UIError.emailInUse)),
+    );
+
+    await sut.signUp();
+  });
+
+  test('Shold emit correct events on UnexpectedError', () async {
+    mockAddAccountError(DomainError.unexpected);
+    sut.validateName(name);
+    sut.validateEmail(email);
+    sut.validatePassword(password);
+    sut.validatePasswordConfirmation(passwordConfirmation);
+
+    expectLater(sut.isLoadingStream, emitsInOrder([true, false]));
+
+    sut.mainErrorStream.listen(
+      expectAsync1((error) => expect(error, UIError.unexpeted)),
+    );
 
     await sut.signUp();
   });
