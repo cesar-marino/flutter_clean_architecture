ef03975cb9e8bea9382a2c7a6ebe4f74304845e0
César Marino (casa) <cesar_marino@live.com>
2020-08-29 14:42:56 -0300

    ensure MinLengthValidation returns error if value is less than min size

--- test/validation/validators/min_length_validation_test.dart
+++ test/validation/validators/min_length_validation_test.dart
@@ -1,3 +1,4 @@
+import 'package:faker/faker.dart';
 import 'package:test/test.dart';
 import 'package:meta/meta.dart';
 
@@ -30,5 +31,12 @@ void main() {
 
   test('Should return error if value is null', () {
     expect(sut.validate(null), ValidationError.invalidField);
+  });
+
+  test('Should return error if value less than min size', () {
+    expect(
+      sut.validate(faker.randomGenerator.string(4, min: 1)),
+      ValidationError.invalidField,
+    );
   });
 }
