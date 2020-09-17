9f7c98d28e8c9f0c9410d9bef1eca3a60cad3b3a
César Marino (casa) <cesar_marino@live.com>
2020-09-17 20:50:54 -0300

    move a duplicated code to a helper method

--- lib/data/usecases/load_surveys/local_load_surveys.dart
+++ lib/data/usecases/load_surveys/local_load_surveys.dart
@@ -19,10 +19,7 @@ class LocalLoadSurveys implements LoadSurveys {
 
       if (data?.isEmpty != false) throw Exception();
 
-      return data
-          .map<SurveyEntity>(
-              (json) => LocalSurveyModel.fromJson(json).toEntity())
-          .toList();
+      return _map(data);
     } catch (error) {
       throw DomainError.unexpected;
     }
@@ -32,12 +29,13 @@ class LocalLoadSurveys implements LoadSurveys {
     final data = await cacheStorage.fetch('surveys');
 
     try {
-      data
-          .map<SurveyEntity>(
-              (json) => LocalSurveyModel.fromJson(json).toEntity())
-          .toList();
+      _map(data);
     } catch (error) {
       await cacheStorage.delete('surveys');
     }
   }
+
+  List<SurveyEntity> _map(List<Map> list) => list
+      .map<SurveyEntity>((json) => LocalSurveyModel.fromJson(json).toEntity())
+      .toList();
 }
